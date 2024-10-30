69ba185c270b0f7cb94eeae0bb8cfd4eb857391d
Dheeraj Chand <dheeraj@dheerajchand.com>
2024-10-29 19:57:02 -0500

    1. Generated JSON works

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ download_and_unzip_gadm:
 		python code/python/2_download_and_unzip_gadm.py
 
 generate_layer_names_and_columns:
-		python code/python/3_fix_gadm.py
+		python code/python/3_generate_layer_names_and_columns.py
 
 python_translate_rasters:
 		python code/python/4_python_translate_rasters.py
--- code/python/3_generate_layer_names_and_columns.py
+++ code/python/3_generate_layer_names_and_columns.py
@@ -3,7 +3,7 @@ __copyright__ = "Siege Analytics, 2024"
 __credits__ = ["Dheeraj Chand"]
 __license__ = "GPL"
 __version__ = "1.0.1"
-__maintainer__ = "Dheeraj Chand
+__maintainer__ = "Dheeraj Chand"
 __email__ = "dheeraj@siegeanalytics.com"
 __status__ = "Production"
 
@@ -15,12 +15,42 @@ from settings import *
 import logging
 import pathlib
 import geopandas as gpd
+import json
+
+def generate_layers_and_attributes(target_gpkg, target_json):
+    
+    # get all layers in 
+    gadm_layers = gpd.list_layers(target_gpkg)['name'].tolist()
+    layer_name_and_attributes = {}
+    
+    # create the dictionary of columns and values
+    for g in gadm_layers:
+
+        gdf = gpd.read_file(target_gpkg,layer=g)
+        layer_name_and_attributes[g] = list(gdf)
+    
+    with open(LAYERS_AND_ATTRIBUTES, 'w') as json_file:
+        json.dump(layer_name_and_attributes, json_file, indent=4)
+
+
+    
+
+
+
+
 
 
 if __name__ == "__main__":
-    logging.info("Step 3: Generate JSON of GADM Layers and Columns")
 
-# CONSTANTS AND MAGICS
+    # CONSTANTS AND MAGICS
+
+    GADM_GPKG = VECTOR_SPATIAL_DATA_SUBDIRECTORY / 'gadm_410-levels' / 'gadm_410-levels.gpkg'
+    LAYERS_AND_ATTRIBUTES = OUTPUT_DIRECTORY / 'layers_and_attributes.json'
+
+    logging.info("Step 3: Generate JSON of GADM Layers and Columns")
+    generate_layers_and_attributes(target_gpkg=GADM_GPKG, target_json=LAYERS_AND_ATTRIBUTES)
+
+
 
 
 
